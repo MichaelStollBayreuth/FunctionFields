c31333c84d9cf807c6af4cae909d900d07adb8fb
Michael Stoll <Michael.Stoll@uni-bayreuth.de>
2024-06-23 20:36:34 +0200

    definition and first example

--- FunctionFields/Basic.lean
+++ FunctionFields/Basic.lean
@@ -1,1 +1,93 @@
-def hello := "world"
+import Mathlib.NumberTheory.FunctionField
+
+/-!
+# Algebraic function fields of one variable
+
+We build on what is already there in `Mathlib.NumberTheory.FunctionField`
+to implement the theory of algebraic function fields of one variable over arbitrary
+base fields.
+
+TODOs relating to `Mathlib.NumberTheory.FunctionField`:
+* Remove mention of finite fields in the documentation as finiteness is nowhere required or used.
+* Move file to somewhere not under `NumberTheory`.
+* `FunctionField` should probably be `IsFunctionField`.
+* Make material universe polymorphic (currently everything is in `Type`).
+
+For now, we set up our own version as a structure `AlgFunctionField1`.
+
+We follow Chapter I of the textbook *Algebraic Function Fields and Codes*
+by Henning Stichtenoth (Springer Universitext, 1993).
+-/
+
+def Algebra.trans {R S A : Type*} [CommRing R] [CommRing S] [CommRing A] [Algebra R S]
+    [Algebra S A] :
+    Algebra R A :=
+  RingHom.toAlgebra <| (algebraMap S A).comp <| algebraMap R S
+
+/-!
+### Definition of algebraic function fields of one variable
+-/
+
+section Def
+
+open scoped IntermediateField
+
+universe u
+
+variable (F : Type u) [Field F]
+
+structure AlgFunctionField1 : Type (u + 1) where
+  /-- The carrier type -/
+  carrier : Type u
+  /-- The carrier type must be a field. -/
+  field : Field carrier
+  /-- The carrier type must be an algebra over the base field. -/
+  algebra : Algebra F carrier
+  /-- An element of the function field that is transcendental over `F` -/
+  x : carrier
+  /-- `x` is transcendental over `F`. -/
+  nonalg : ¬ IsAlgebraic F x
+  /-- The function field is finite-dimensional over the rational function field `F(x)`. -/
+  finite_dim : FiniteDimensional F⟮x⟯ carrier
+
+attribute [instance] AlgFunctionField1.field AlgFunctionField1.algebra
+
+lemma RatFunc.aeval_X_eq_algebraMap (p : Polynomial F) :
+    Polynomial.aeval X p = algebraMap _ (RatFunc F) p := by
+  rw [← algebraMap_X, Polynomial.aeval_algebraMap_apply, Polynomial.aeval_X_left_apply]
+
+/-- The rational function field in one variable as an algebraic function field in one variable. -/
+noncomputable
+def RatFunc.toAlgFunctionField1 : AlgFunctionField1 F where
+  carrier := RatFunc F
+  field := inferInstance
+  algebra := inferInstance
+  x := X
+  nonalg := by
+    rw [isAlgebraic_iff_not_injective, not_not, injective_iff_map_eq_zero]
+    intro p
+    simp only [aeval_X_eq_algebraMap, algebraMap_eq_zero_iff, imp_self]
+  finite_dim := by
+    have : F⟮X⟯ = (⊤ : IntermediateField F (RatFunc F)) := by
+      refine IntermediateField.ext fun a ↦ ⟨fun _ ↦ trivial, fun _ ↦ ?_⟩
+      rw [IntermediateField.mem_adjoin_simple_iff]
+      exact ⟨a.num, a.denom, by simp only [aeval_X_eq_algebraMap, num_div_denom]⟩
+    exact this ▸ Module.finite_of_rank_eq_one IntermediateField.rank_top
+
+end Def
+
+namespace AlgFunctionField1
+
+variable {F : Type*} [Field F] (FF : AlgFunctionField1 F)
+
+/-- The *field of constants*  of an algebraic function field `FF` of one variable over `F`
+is the relative algebraic closure of `F` in `FF`. -/
+def fieldOfConstants : Subalgebra F FF.carrier := integralClosure F FF.carrier
+
+/-- An algebraic function field of one variable is *geometric* if its field of constants
+is the base field. -/
+def IsGeometric : Prop := FF.fieldOfConstants = ⊥
+
+
+
+end AlgFunctionField1
